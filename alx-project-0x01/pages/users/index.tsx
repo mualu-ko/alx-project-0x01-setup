8e16c5f832bea8e7a652cb5c329592c49cbf28a5
mualu-ko <kmwaluko.wambua@gmail.com>
2025-09-15 16:54:27 +0300

    task 3

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,30 @@
-import Header from "@/components/layout/Header";
+import React from "react";
+import { UserProps } from "@/interfaces";
+import UserCard from "@/components/common/UserCard";
+
+interface UsersPageProps {
+  posts: UserProps[]; // posts is actually users from the API
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-8">
+      {posts.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
